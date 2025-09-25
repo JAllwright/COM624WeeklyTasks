7b314b833cee7abe6206292939ca91c85bf4d23a
JAllwright <0allwj75@solent.ac.uk>
2025-09-25 17:03:30 +0100

    Initial commit

---
+++ week1/cleandataset.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv('titanic_samples.csv')
+
+df.info()
+
+df.isnull().sum()
+
+df.drop(['Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
+
+df.fillna({'Age': df['Age'].median()}, inplace=True)
+
+df.to_csv('titanic_cleaned.csv', index=False)
