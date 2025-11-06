e524aea5b8330ef52064e2636c69c0cffbfd99c8
JAllwright <0allwj75@solent.ac.uk>
2025-11-06 14:41:00 +0000

    Steps 1-3

---
+++ week5/week5.py
@@ -0,0 +1,11 @@
+from pandas import read_csv
+from numpy import set_printoptions
+from sklearn import preprocessing
+
+header_names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+my_data = read_csv("pima_indians_diabetes_2.csv", names=header_names)
+
+print(my_data.shape)
+
+print(my_data.head())
+print(my_data.tail())
